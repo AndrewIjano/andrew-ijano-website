212293f41ba4ebc42825db4b8e6aa600bbf0e9e1
Andrew Ijano <andrew.lopes@uol.com.br>
2019-08-26 16:35:03 -0300

    Add headline animation

--- src/components/Headline.vue
+++ src/components/Headline.vue
@@ -1,13 +1,31 @@
 <template>
   <div id="headline">
-    <img class="user-picture" src="../assets/user-picture.jpg" alt />
-    <h1 class="title">{{msg}}</h1>
+    <img class="headline--user-picture" src="../assets/user-picture.jpg" alt />
+    <h1 class="headline--title">{{msg}}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: "Headline",
+  mounted() {
+    let tl = this.$anime.timeline({
+      easing: "easeOutExpo"
+    });
+
+    tl.add({
+      targets: ".headline--title",
+      opacity: [0, 1],
+      scale: [0.2, 1],
+      duration: 1400
+    });
+
+    tl.add({
+      targets: ".headline--user-picture",
+      opacity: [0, 1],
+      duration: 1200
+    });
+  },
   props: {
     msg: String
   }
@@ -23,13 +41,13 @@ export default {
   align-items: center;
   justify-content: center;
 }
-.title {
+.headline--title {
   color: white;
   font-size: 4em;
   font-weight: bold;
 }
 
-.user-picture {
+.headline--user-picture {
   max-height: 60vh;
   width: auto;
   border-radius: 50%;
@@ -37,11 +55,11 @@ export default {
 }
 
 @media (max-width: 800px) {
-  .user-picture {
+  .headline--user-picture {
     max-width: 70vw;
   }
 
-  .title {
+  .headline--title {
     font-size: 2em;
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAnime from 'vue-animejs';
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.use(VueAnime);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
